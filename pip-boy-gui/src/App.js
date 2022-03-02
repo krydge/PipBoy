9c76f3b8fe6fda7f77f64e3c1b1ad44dd293a86d
Kaydon Stubbs <KaydonS@christensenarms.com>
2022-03-01 21:44:34 -0700

    vhs filter

--- pip-boy-gui/src/App.js
+++ pip-boy-gui/src/App.js
@@ -13,40 +13,48 @@ function App() {
   }
   return (
     <div className="App">
-      {page===pages[0]&&<>
-      <div>{page}</div>
-      <div className="StatsSubPages">
-        {statsSubPages?.map((subPage) =>
-          <>
-            <button>{subPage}</button>
-          </>)}
-      </div>
-      <div className="StatsBar">
-        <h3>LVL {Stats.LVL}</h3>
-        <h3>HP {Stats.HPCUR + "/" + Stats.HPMAX}</h3>
-        <h3>AP {Stats.APAVAILABLE + "/" + Stats.APMAX}</h3>
-        <h3>XP {Stats.XPERNED + "/" + Stats.XPNEEDED}</h3>
-      </div>
-      <div className='StatsImage'>
-        <img src={statsimg}></img>
-        <h2>{Stats.PLayerName + " - LEVEL" + Stats.LVL}</h2>
-      </div>
-      <div className='NavBar'>
-        {pages?.map((mainPage) =>
-          <>
-            <button onClick={changePage} value={mainPage}>{mainPage}</button>
-          </>)}
-      </div>
+      {page === pages[0] && <>
+        <div>{page}</div>
+        <div className="StatsSubPages">
+          {statsSubPages?.map((subPage) =>
+            <>
+              <button key={subPage}>{subPage}</button>
+            </>)}
+        </div>
+        <div className="StatsBar">
+          <div className="separator">____________</div>
+          <h3>LVL {Stats.LVL}</h3>
+          <div className="separator">____________</div>
+          <h3>HP {Stats.HPCUR + "/" + Stats.HPMAX}</h3>
+          <div className="separator">____________</div>
+          <h3>AP {Stats.APAVAILABLE + "/" + Stats.APMAX}</h3>
+          <div className="separator">____________</div>
+          <h3>XP {Stats.XPERNED + "/" + Stats.XPNEEDED}</h3><div className="separator">____________</div>
+        </div>
+        <div className='StatsImage'>
+          <img src={statsimg}></img>
+          <h2>{Stats.PLayerName + " - LEVEL" + Stats.LVL}</h2>
+        </div>
+        <div className='NavBar'>
+        <div className="separator">____________</div>
+          {pages?.map((mainPage) =>
+            <>
+              <button onClick={changePage} value={mainPage} key={mainPage}>{mainPage}</button>
+              <div className="separator">____________</div>
+            </>)}
+        </div>
       </>
-}
-{page!=pages[0]&&<>
-      <div>{page}</div>
-      <div className='NavBar'>
-        {pages?.map((mainPage) =>
-          <>
-            <button onClick={changePage} value={mainPage}>{mainPage}</button>
-          </>)}
-      </div>
+      }
+      {page != pages[0] && <>
+        <div>{page}</div>
+        <div className='NavBar'>
+          <div className="separator">____________</div>
+          {pages?.map((mainPage) =>
+            <>
+              <button onClick={changePage} value={mainPage} key={mainPage}>{mainPage}</button>
+              <div className="separator">____________</div>
+            </>)}
+        </div>
       </>}
     </div>
   );
