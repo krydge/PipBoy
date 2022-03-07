dab85b10efb2b1e3b9ef3f91b81eaaaa03b823fa
Kaydon Stubbs <KaydonS@christensenarms.com>
2022-03-07 15:02:41 -0700

    started to create character madel

---
+++ pip-boy-gui/src/Models/characteModel.js
@@ -0,0 +1,12 @@
+class Character{
+constructor  (){
+    this.LVL = 1
+    this.HPMAX = 100*LVL*.7
+    this.HPCUR = this.HPMAX
+    this.APMAX = 100*LVL*.5
+    this.APAVAILABLE = this.APMAX
+    this.XPERNED = 0
+    this.XPNEEDED = this.LVL*100
+
+}
+}
